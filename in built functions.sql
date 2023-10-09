83c84b00747d0d03da8953cee7ff48719670c3e7
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2023-10-09 08:36:51 +0530

    in built string functions added

--- in built functions.sql
+++ in built functions.sql
@@ -1,2 +1,8 @@
+-------------------------------------- CONCAT() --------------------------------------
+
 SELECT CONCAT('Hello', 'World');		-- o/p : "HelloWorld"
+SELECT CONCAT('Hello', ' ', 'World');		-- o/p : "Hello World"
+
+SELECT CONCAT(first_name, ' ', last_name) as Employee_Full_Name FROM dev_schema.employee;
+
 
