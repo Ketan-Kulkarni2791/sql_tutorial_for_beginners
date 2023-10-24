d027595c3934daa7ee8e1c74310596d071e14573
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2023-10-24 16:03:52 +0530

    string functions added

--- in built functions.sql
+++ in built functions.sql
@@ -21,7 +21,13 @@ SELECT LOWER('GDWLKGHDSLGHDG');					-- o/p : "gdwlkghdslghdg"
 SELECT LOWER(first_name) as full_name FROM dev_schema.employee;
 SELECT LOWER(CONCAT(first_name, ' ', last_name)) as full_name FROM dev_schema.employee;
 
+-------------------------------------- CHAR_LENGTH()/CHARACTER_LENGTH() --------------------------------------
 
--------------------------------------- LPAD() --------------------------------------
+SELECT CHAR_LENGTH('hjdghdsjhgjahsd');						-- o/p : 15
+SELECT CHAR_LENGTH('hjdghdsjhgjahsd kgkkgk yfhdj');			-- o/p : 28
+SELECT CHARACTER_LENGTH('hjdghdsjhgjahsd');					-- o/p : 15
+SELECT CHARACTER_LENGTH('hjdghdsjhgjahsd kgkkgk yfhdj');	-- o/p : 28
 
-SELECT LPAD('')
+SELECT last_name, CHAR_LENGTH(last_name) as length_of_last_names FROM dev_schema.employee;
+
+
